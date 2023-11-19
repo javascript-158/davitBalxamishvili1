b80e63662bfd0a9e4457eef84432380ac670ee95
dato201 <146935809+dato201@users.noreply.github.com>
2023-11-19 16:04:35 +0400

    add script.js dan

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let user = prompt("enter your name end last name");
+let age = prompt("Enter your date of birth");
+let Dwelling = prompt("Enter your place of residence");
+
+
+alert("გამარჯობა "+ user+ " მიხარია რომ უკვე "+ (age - 2023)+ " წლის ხარ და "+ Dwelling+ " -ში ცხოვრობ.");
